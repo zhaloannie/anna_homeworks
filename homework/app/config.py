f8305c616ede554c642e6a1ffcc28a96ac6bdf1f
John Doe <john@email.com>
2026-01-06 16:01:40 +0100

    homework10

---
+++ homework/app/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_USERNAME = os.getenv("REDIS_USERNAME")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
